2db86c2c65f9d4458a363135c7a19e64325f1aa8
Gabi M <solo.games.9@hotmail.com>
2021-02-14 06:57:08 -0300

    first

---
+++ base/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+
+
+class Tag(models.Model):
+	name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
+
+class Post(models.Model):
+    headline = models.CharField(max_length=200)
+    sub_headline = models.CharField(max_length=200, null=True, blank=True)
+    thumbnail = models.ImageField(null=True, blank=True, upload_to="images", default="placeholder.png")
+    body = RichTextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
+    slug = models.SlugField(null=True, blank=True)
+ 
+    def __str__(self):
+        return self.headline
+    
+    def save(self, *args, **kwargs):
+        """
+        Overwrite save method for slug field.
+        """
+        if self.slug == None:
+            slug = slugify(self.headline)
+
+            has_slug = Post.objects.filter(slug=slug).exists()
+            count = 1
+            while has_slug:
+                count += 1
+                slug = slugify(self.headline) + '-' + str(count) 
+                has_slug = Post.objects.filter(slug=slug).exists()
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+    
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+	first_name = models.CharField(max_length=200, blank=True, null=True)
+	last_name = models.CharField(max_length=200, blank=True, null=True)
+	email = models.CharField(max_length=200)
+	profile_pic = models.ImageField(null=True, blank=True, upload_to="images", default="/user.png")
+	bio = models.TextField(null=True, blank=True)
+	twitter = models.CharField(max_length=200,null=True, blank=True)
+
+	def __str__(self):
+		name = str(self.first_name)
+		if self.last_name:
+			name += ' ' + str(self.last_name)
+		return name
+
---
+++ base/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
+# Create your views here.
+from .models import Post
+
+
+def home(request):
+    posts = Post.objects.filter(active=True, featured=True)[0:3]
+    context = {'posts': posts}
+    return render(request, 'base/index.html', context)
+
+def posts(request):
+    posts = Post.objects.filter(active=True)
+    context = {'posts': posts}
+    return render(request, 'base/posts.html', context)
+
+def post(request, slug):
+    post = Post.objects.get(slug=slug)
+    context = {'post': post}
+    return render(request, 'base/post.html', context)
+
+def profile(request):
+    return render(request, 'base/profile.html')
+
+def sendEmail(request):
+    if request.method == 'POST':
+        template = render_to_string('base/email_template.html',{
+            'name' : request.POST['name'],
+            'email' : request.POST['email'],
+            'message' : request.POST['message'],
+        })
+        
+        email = EmailMessage(
+            request.POST['subject'],
+            template,
+            settings.EMAIL_HOST_USER,
+            ['moreyragabriel97@gmail.com']
+        )
+        email.fail_silently=False
+        email.send()
+    return render(request, 'base/email_sent.html')
